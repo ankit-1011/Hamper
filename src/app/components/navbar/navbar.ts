c0428b50c1706ad958d8020647d0951a7323ca9b
Ankit Mishra <ankitmishra16127@gmail.com>
2025-12-02 08:35:22 +0530

    responive

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,24 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 import { ShoppingCart } from 'lucide-angular/src/icons';
 import { AuthService } from '../../services/login';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, LucideAngularModule,ReactiveFormsModule,FormsModule], 
+  imports: [RouterLink, LucideAngularModule, ReactiveFormsModule, FormsModule,CommonModule],
   templateUrl: './navbar.html',
-  styleUrls: ['./navbar.css'],              
+  styleUrls: ['./navbar.css'],
 })
 export class NavbarComponent {
 
+  isOpen = false;
+
+  constructor(private router: Router,public auth: AuthService) {}
+
   readonly shoppingCart = ShoppingCart
 
-  constructor(public auth: AuthService) {}
-
- logout() {
+  logout() {
     this.auth.logout();
   }
+
+ reload() {
+  this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+    this.router.navigate(['/products']);
+  });
+}
+
 }
